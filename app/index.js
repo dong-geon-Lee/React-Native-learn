ebb8a03e2f0a2da9e7fd219769fa83179c139298
developer <ch9716@naver.com>
2023-12-07 21:01:30 +0900

    React Native 재설정

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from "react-native";
+import React, { useState } from "react";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
